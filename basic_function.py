14dcf9e3d02111a4dd0a4cc75722fb7b1601eadc
MarkS05 <somidomarkallen_bsit@plmun.edu.ph>
2025-02-19 16:47:22 +0800

    Submitting Python Week 04 - Laboratory # 16

---
+++ basic_function.py
@@ -0,0 +1,9 @@
+# Filename: basic_function.py
+
+# Step 1: Define the function
+def greet():
+    """Prints a greeting message."""
+    print("Hello, Python Students!")  # Function body
+
+# Step 2: Call the function
+greet()
